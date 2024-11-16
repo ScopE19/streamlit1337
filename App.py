1211d3c7224faf85d9c7a57384439a0939fc3d2a
Danila Oleinikov <95969385+ScopE19@users.noreply.github.com>
2024-11-16 15:42:40 +0000

    123

--- App.py
+++ App.py
@@ -1,31 +1,54 @@
 import streamlit as st
+import os
 from pydeck.bindings.map_styles import styles
 from Pages import Home,Project1, Project2, Project3
 from streamlit_navigation_bar import st_navbar as navbar
+from PIL import Image
+import pandas as pd
 
-st.set_page_config(initial_sidebar_state='collapsed')
+
+image = Image.open('./src/jinmark.png')
+st.set_page_config(initial_sidebar_state='collapsed', page_icon=image)
 pages = ["Home","Project1","Project2","Project3"]
 styles = {
     "nav": {
-        "background-color": "rgb(123, 209, 146)"
+        "background-color": "red",
+        "font-size": "1.5rem",
+        "font-family": "Georgia, bold",
+        "display": "flex",
+        "justify-content": "center"
+        
+    },
+    "img": {
+        "position": "absolute",
+        "left": "0px",
+        "font-size": "15px",
+        "top": "5px",
+        "width": "200px",
+        "height": "40px",
     },
     "div": {
         "max-width": "32rem"
     },
     "span": {
-        "border-radius": "0.5rem",
+        "border-radius": "0.2rem",
         "color": "rgb(49,51,63)",
         "margin": "0 0.125rem",
-        "padding": "0.4375rem 0.625rem",
+        "padding": "0.6275rem 0.625rem",
     },
     "active": {
-        "background-color": "rgba(105, 114, 255, 0.25)",
+        "background-color": "rgba(105, 114, 255, 0.4)",
+        "color": "rgba(255, 255, 255)"
+    
     },
     "hover": {
-        "background-color": "rgba(255, 255, 255, 0.35)",
+        "background-color": "rgba(0, 0, 0, 0.7)",
+        "color": "rgba(255, 255, 255)",
+    
     },
 }
-page = navbar(pages, styles= styles)
+logo_path = os.path.join(os.path.dirname(__file__), "src", "tekkenlogo.svg")
+page = navbar(pages, styles= styles, logo_path=logo_path,)
 if page == 'Home':
     Home.Home().app()
 elif page == 'Project1':
@@ -37,3 +60,4 @@ elif page == 'Project3':
 else:
     Home.Home().app()
 st.title('hi')
+st.image('./src/monke.png')
